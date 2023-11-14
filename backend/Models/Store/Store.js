d5333c39523d0ec7b15ce73a558741ba32354603
tasha787 <tashahafeez787@gmail.com>
2023-11-14 13:48:17 +0500

    Added Store Model

---
+++ backend/Models/Store/Store.js
@@ -0,0 +1,53 @@
+const { Sequelize } = require("sequelize");
+const { sequelize, DataTypes } = require("../../config/config");
+
+  const Store = sequelize.define('stores', 
+  {
+    storeID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+   
+    ownerEmail:{
+      type: DataTypes.STRING,
+      allowNull:false,
+      unique:true
+    },
+    storeName:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    storeAddress:{
+      type: DataTypes.STRING,
+      allowNull:false,
+      unique:true
+    },
+    storeContact:{
+      type: DataTypes.STRING,
+      allowNull:false,
+      unique:true
+   
+    },
+    storeType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    startTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    endTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    availability: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    
+  });
+  sequelize.sync().then(()=>{console.log('created store')}).catch()
+
+  module.exports = Store;
