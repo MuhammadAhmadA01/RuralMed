910ed350737d0d871daa7c5b4274d166ce8260e0
Usamashahid112 <usamashahidshahid52@gmail.com>
2023-11-14 13:41:20 +0500

    Added Owner Model

---
+++ backend/Models/Owner/Owner.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require("sequelize");
+const { sequelize, DataTypes } = require("../../config/config");
+const owner = sequelize.define('owners', {
+  ownerID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+ 
+  cnic: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email:{
+    type: DataTypes.STRING,
+    allowNull:false,
+    unique:true
+  },
+  
+});
+sequelize.sync().then(()=>{console.log('created owner')}).catch()
+module.exports = owner;
