cf4db727ea787b0620966aff2ed818d7422ccc98
tasha787 <tashahafeez787@gmail.com>
2023-11-14 13:31:17 +0500

    Added Customer Model

---
+++ backend/Models/Customer/Customer.js
@@ -0,0 +1,29 @@
+
+const { Sequelize } = require("sequelize");
+const { sequelize, DataTypes } = require("../../config/config");
+const Customer = sequelize.define('customers', {
+  customerID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  email:{
+    type: DataTypes.STRING,
+    allowNull:false,
+    unique:true
+  },
+  cnic: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  deliveryFee: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  
+  
+});
+sequelize.sync().then(()=>{console.log('created customer')}).catch()
+module.exports = Customer;
