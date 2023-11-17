cec42aa2f42a4f94a6f529d1b5fe45038bdb2069
tasha787 <tashahafeez787@gmail.com>
2023-11-17 14:17:59 +0500

    Added Model of Product

---
+++ backend/Models/Product/products.js
@@ -0,0 +1,33 @@
+const { Sequelize } = require("sequelize");
+const { sequelize, DataTypes } = require("../../config/config");
+
+const Product = sequelize.define("products", {
+  productID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  storeId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  availableQuantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
