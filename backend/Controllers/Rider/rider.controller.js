6b7507f8788c1279045c358ed0fff963afa3e24c
tasha787 <tashahafeez787@gmail.com>
2023-11-21 00:45:40 +0500

    Modified Rider controller to view his profile

--- backend/Controllers/Rider/rider.controller.js
+++ backend/Controllers/Rider/rider.controller.js
@@ -29,4 +29,49 @@ const createRider = (req, res) => {
         res.status(status).json({ error: error.message || 'Internal Server Error' });
   });
 };
-module.exports = { createRider };
+const viewProfile = (req, res) => {
+  const { email } = req.params;
+  // Retrieve rider record from the 'riders' table
+  Riders.findOne({
+    where: { email: email },
+    attributes: { exclude: ["id"] },
+  })
+    .then((riderRecord) => {
+      if (!riderRecord) {
+        return res
+          .status(404)
+          .json({ error: "rider not found with this email." });
+      }
+
+      // Retrieve user record from the 'users' table
+      USER.findOne({
+        where: { email: email },
+        attributes: { exclude: ["password", "role"] },
+      })
+        .then((userRecord) => {
+          if (!userRecord) {
+            return res
+              .status(404)
+              .json({ error: "rider not found with this email." });
+          }
+
+          // Combine and send the response
+          const combinedResponse = {
+            rider: riderRecord.toJSON(),
+            user: userRecord.toJSON(),
+          };
+
+          res.status(200).json(combinedResponse);
+        })
+        .catch((error) => {
+          console.error("Error retrieving user record:", error);
+          res.status(500).json({ error: "Internal Server Error" });
+        });
+    })
+    .catch((error) => {
+      console.error("Error retrieving rider record:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+};
+
+module.exports = { createRider,viewProfile };
