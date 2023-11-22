e40e1caf7fe8e934e19630cefc68d660b153653e
tasha787 <tasha787@gmail.com>
2023-11-22 13:54:02 +0500

    Added multer configuration

---
+++ backend/multer/multer.js
@@ -0,0 +1,13 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb('invalid image file!', false);
+  }
+};
+const uploads = multer({ storage, fileFilter });
+module.exports={uploads}
