7aa624597979cca10b1e0582b46a08780a059872
usamashahid112 <usamashahidshahid52@gmail.com>
2023-11-22 14:17:14 +0500

    Modified Owner middleware

--- backend/middlewares/ownerMiddleware.js
+++ backend/middlewares/ownerMiddleware.js
@@ -1,8 +1,8 @@
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
 const ownerValidationRules = [
-  body('cnic').isNumeric().isLength({ min: 13, max: 13 }).notEmpty(),
-  body('email').isEmail().notEmpty(),
+  body("cnic").isNumeric().withMessage('Cnic must be numeric').isLength({ min: 13, max: 13 }).withMessage('Cnic must be of length 13').notEmpty().withMessage('Cnic should not be empty'),
+  body("email").isEmail().withMessage('Invalid email format').notEmpty().withMessage('Email must not be empty'),
 ];
 const validateOwner = (req, res, next) => {
   const errors = validationResult(req);
