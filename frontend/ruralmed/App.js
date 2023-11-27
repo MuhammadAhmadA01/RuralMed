7f61628b4a90c204c8bc0e3c6b1122243e9051da
MuhammadAhmada01 <aimmichamps@gmail.com>
2023-11-28 02:58:58 +0500

    Defined Entry point file (App.js)

--- frontend/ruralmed/App.js
+++ frontend/ruralmed/App.js
@@ -1,20 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CustomerScreen from './Screens/Customer/CustomerInputScreen';
+import LoginScreen from './Screens/User/UserLoginScreen';
+import RiderScreen from './Screens/Rider/RiderInputScreen';
+import StoreOwnerScreen from './Screens/Owner/StoreOwnerScreen';
+import CustomerHomeScreen from './Screens/Customer/CustomerHomeScreen';
+import OwnerHomeScreen from './Screens/Owner/OwnerHomeScreen';
+import Signup from './Screens/User/UserSignupScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
 
- 
+  // Customize the theme with your preferred colors
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#25d366', // Replace with your primary color
+       },
+  };
+
   return (
-    <View style={styles.container}>
-      </View>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="signup" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="signup" component={Signup} />
+          <Stack.Screen name="Customer" component={CustomerScreen} />
+          <Stack.Screen name="login" component={LoginScreen} />
+          <Stack.Screen name="Rider" component={RiderScreen} />
+          <Stack.Screen name="Owner" component={StoreOwnerScreen} />
+          <Stack.Screen name="HomeCustomer" component={CustomerHomeScreen} />
+          <Stack.Screen name="HomeOwner" component={OwnerHomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
