0d518cedf623277b1b05e9863f9398e720425ae4
usamashahid112 <usamashahidshahid52@gmail.com>
2023-12-06 15:02:57 +0500

    Added switch for availability on rider's screen

--- frontend/ruralmed/Screens/Rider/RiderHomeScreen.js
+++ frontend/ruralmed/Screens/Rider/RiderHomeScreen.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { View, ScrollView, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, StatusBar, Switch } from 'react-native';
 import { Appbar, Card, Title, Paragraph, Menu } from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import AppHeaderRider from '../../Components/RiderAppHeader';
-
+import IP_ADDRESS from '../../config/config';
 const RiderHomeScreen = ({ navigation }) => {
   // Replace these values with the actual data you want to display
   const prevMonthEarning = 1500;
@@ -11,6 +11,68 @@ const RiderHomeScreen = ({ navigation }) => {
   const currentMonthEarning = 2000;
   const currentMonthOrders = 40;
   const inProgressOrders = 5;
+  const [availabilitySwitch, setAvailabilitySwitch] = useState(false);
+  const [riderEmail, setRiderEmail] = useState('');
+
+  useEffect(() => {
+    // Fetch rider's profile to get availability status
+    const fetchRiderProfile = async () => {
+      try {
+        // Get rider's email from AsyncStorage
+        const phone = await AsyncStorage.getItem('phone');
+        
+        // Get rider's email from the server
+        const emailResponse = await fetch(`http://${IP_ADDRESS}:5000/get-email`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            contactNumber: phone,
+          }),
+        });
+
+        const { email } = await emailResponse.json();
+        setRiderEmail(email);
+
+        // Fetch rider's profile based on the obtained email
+        const profileResponse = await fetch(`http://${IP_ADDRESS}:5000/get-profile/${email}`);
+        console.log(profileResponse)
+        const { rider } = await profileResponse.json();
+        //console.log(rider)
+        // Update the availability switch based on the availability status
+        if (rider && rider.availabilityStatus==='Online') {
+          setAvailabilitySwitch(true);
+        }
+        else
+        if(rider.availabilityStatus==='Offline')
+        {
+          setAvailabilitySwitch(false);
+
+        }
+      } catch (error) {
+        console.error('Error fetching rider profile:', error);
+      }
+    };
+
+    fetchRiderProfile();
+  }, []);
+  const updateAvailabilityStatus = async (status) => {
+    console.log(riderEmail)
+    try {
+      const updateStatusResponse = await fetch(`http://${IP_ADDRESS}:5000/${riderEmail}/${status}`, {
+        method: 'PUT',
+      });
+
+      if (updateStatusResponse.ok) {
+        console.log(`Availability status updated to ${status}`);
+      } else {
+        console.error('Error updating availability status');
+      }
+    } catch (error) {
+      console.error('Error updating availability status:', error);
+    }
+  };
 
   return (
     <View style={{ flex: 1 }}>
@@ -19,7 +81,21 @@ const RiderHomeScreen = ({ navigation }) => {
       <View style={{ padding: 16, alignItems: 'center' }}>
         <Title style={{ color: 'black', fontSize: 24, fontWeight: 'bold' }}>Your Monthly Dashboard</Title>
       </View>
-
+      <View style={{ padding: 16, flexDirection: 'row', alignItems: 'center' }}>
+        <Title style={{ color: 'black', fontSize: 18 }}>Availability Status</Title>
+        <Switch
+          value={availabilitySwitch}
+          onValueChange={(value) => {
+            setAvailabilitySwitch(value);
+            const status = value ? 'Online' : 'Offline';
+            updateAvailabilityStatus(status);
+          }}
+          style={{ marginLeft: '45%' }}
+          thumbColor={availabilitySwitch ? "#25d366" : "#9E9E9E"}
+      
+          
+        />
+      </View>
       {/* Dashboard Items */}
       <ScrollView contentContainerStyle={{ padding: 16 }}>
         <Card style={{ marginBottom: 16, flex: 1, height: 130 }}>
