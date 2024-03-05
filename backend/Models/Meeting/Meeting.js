70c27e990173ac6bae09cd09019bd32f20d6d143
tasha787 <tashahafeez77@gmail.com>
2024-03-05 14:08:38 +0500

    Added Model for Meeting

---
+++ backend/Models/Meeting/Meeting.js
@@ -0,0 +1,51 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const { sequelize } = require('../../config/config'); // Assuming you have a database configuration file
+
+const Meeting = sequelize.define('Meetings', {
+  meetingID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  meetingFee: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      max: 2000 
+    }
+  },
+  scheduledDate: {
+    type: DataTypes.STRING,
+    allowNull:false
+  },
+  startTime: {
+    type: DataTypes.TIME,
+    allowNull:false 
+  },
+  endTime: {
+    type: DataTypes.TIME,
+    allowNull:false 
+  },
+  customerId:{
+    type:DataTypes.STRING,
+    allowNull:false
+  },
+dvmId:{
+  type:DataTypes.STRING,
+  allowNull:false
+},
+status:{
+  type:DataTypes.STRING,
+  allowNull:false
+}
+});
+
+sequelize.sync()
+  .then(() => {
+    console.log('Meeting model synced with database');
+  })
+  .catch((error) => {
+    console.error('Error syncing DVM model with database:', error);
+  });
+
+module.exports = Meeting;
