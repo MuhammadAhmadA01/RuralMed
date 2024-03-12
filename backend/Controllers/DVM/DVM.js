32f5d8cc7f28842d862753ca92bf650debb6d101
usamashahid112 <usamashahidshahid52@gmail.com>
2024-03-12 22:58:21 +0500

    Added Controller for getting all Dvms

--- backend/Controllers/DVM/DVM.js
+++ backend/Controllers/DVM/DVM.js
@@ -26,5 +26,36 @@ const addDVM = async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 };
+
+const getAllDVMs = async (req, res) => {
+  try {
+    // Fetch all users where role is "DVM"
+    const allUsers = await User.findAll({ where: { role: 'Dvm' } });
+
+    // Fetch all DVM records from the database
+    const allDVMs = await DVM.findAll();
+    // Combine DVMs with additional user information
+    const combinedDVMs = allDVMs.map((dvm) => {
+      // Find corresponding user for the DVM by matching email
+      const correspondingUser = allUsers.find((user) => user.email === dvm.email);
+      // Add additional fields from the user to the DVM object
+      return {
+        ...dvm.toJSON(),
+       picture: correspondingUser.picture,
+        cityNearBy: correspondingUser.cityNearBy,
+        name: `${correspondingUser.firstName} ${correspondingUser.lastName}`
+      };
+
+    });
+    console.log(combinedDVMs)
+    // Send the combined DVMs as a response
+    res.json(combinedDVMs);
+  } catch (error) {
+    // If an error occurs, send an error response
+    console.error('Error fetching all DVMs:', error);
+    res.status(500).json({ error: 'An error occurred while fetching DVMs' });
+  }
+};
+
 // Export the controller function
-module.exports = { addDVM};
+module.exports = { addDVM, getAllDVMs};
