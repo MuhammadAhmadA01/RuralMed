6f5ef52a880bc525630f22c56b4ad097ce3abc31
MuhammadAhmadA01 <mahmad.8962@gmail.com>
2024-03-24 17:46:13 +0500

    Modified Screens of Placing order and booking meeting to send emails

--- frontend/ruralmed/Screens/Customer/PlaceOrder/PlaceOrderScreen.js
+++ frontend/ruralmed/Screens/Customer/PlaceOrder/PlaceOrderScreen.js
@@ -1,4 +1,3 @@
-// PlaceOrderScreen.js
 import React, { useState, useEffect } from "react";
 import styles from "./styles/styles";
 import {
@@ -8,12 +7,15 @@ import {
   TouchableOpacity,
   StyleSheet,
   ActivityIndicator,
+  Alert,
+  Image,
 } from "react-native";
-import { Appbar, Button, Title } from "react-native-paper";
+import { Appbar, Button, Title, RadioButton } from "react-native-paper";
 import { useSelector, useDispatch } from "react-redux";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import IP_ADDRESS from "../../../config/config";
 import { clearCart } from "../../../Components/Cart/CartSlice";
+import * as ImagePicker from "expo-image-picker";
 
 const PlaceOrderScreen = ({ navigation, route }) => {
   const { customerContact } = route.params;
@@ -21,11 +23,124 @@ const PlaceOrderScreen = ({ navigation, route }) => {
   const cartItems = useSelector((state) => state.cart.cartItems);
   const storeID = useSelector((state) => state.cart.storeInfo);
   const [loading, setLoading] = useState(false);
+  const [isPaymentUploaded, setIsPaymentUploaded] = useState(false); // New state variable for tracking payment upload
+  const [paymentVerified, setPaymentVerified] = useState(false); // New state variable for tracking payment verification
+
   const [riderDistances, setRiderDistances] = useState({});
   const [assignedRider, setAssignedRider] = useState({});
   const [store, setStore] = useState({});
   const [customerEmail, setCustomerEmail] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("COD"); // Default to Cash on Delivery
+  const [isPlaceOrderDisabled, setIsPlaceOrderDisabled] = useState(false); // New state variable
 
+  const [paymentProofImage, setPaymentProofImage] = useState(null); // State to store the selected image
+
+  // Function to handle uploading picture from gallery
+  const handleUploadPicture = async () => {
+    try {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        throw new Error(
+          "Sorry, we need camera roll permissions to make this work!"
+        );
+      }
+
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        const { uri } = result;
+        const name = new Date().getTime() + "_payment";
+        const type = "image/jpg";
+        const newFormData = new FormData();
+        newFormData.append("paymentProof", { uri, name, type });
+        setPaymentProofImage(uri);
+        //      setFormData(newFormData);
+        setIsPaymentUploaded(true);
+      }
+    } catch (error) {
+      Alert.alert(
+        "Error",
+        "An error occurred during image selection. Please try again."
+      );
+    }
+  };
+  const handleVerifyPayment = () => {
+
+      if(paymentMethod!=="COD")
+      {
+      const apiKey = "voLMDc1Disq5hRxNkhcHGg==5cat5sdYb3qGjkDV";
+      const apiUrl = "https://api.api-ninjas.com/v1/imagetotext";
+  
+      const name = new Date().getTime() + "_payment";
+      const type = "image/jpeg";
+      const formData = new FormData();
+      formData.append("image", { name, uri: paymentProofImage, type });
+      
+      fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "multipart/form-data",
+          "X-Api-Key": apiKey,
+        },
+        body: formData,
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        const currentDate = new Date();
+        const extractedText = data
+          .map((item) => item.text.toLowerCase())
+          .join(" ");
+        if (
+          extractedText.includes("asiya bano") &&
+          (extractedText.includes("transferred to other wallet") ||
+            extractedText.includes("transaction successful money has been sent") ||
+            extractedText.includes("transferred to other jazzcash")) &&
+          extractedText.includes(
+            calculateOrderTotal(
+              calculateSubtotal(),
+              assignedRider.fee
+            ).toString() + ".00"
+          ) &&
+          (extractedText.includes("*******5512") ||
+            extractedText.includes("03217645512")) &&
+          extractedText.includes(
+            currentDate
+              .toLocaleString("default", { month: "long" })
+              .toLocaleLowerCase()
+          ) &&
+          extractedText.includes("2024")
+        ) {
+          setPaymentVerified(true);
+          
+          console.log("Payment verified");
+          placeOrder()
+        } else {
+          console.log("Payment verification failed");
+          Alert.alert(
+            "Verification Failed",
+            "Please Make sure the following things in your Payment Screenshot:\nThe Receiver's Account # should be visible\nThe transaction is made today\nThe transaction must have the amount equal to order total"
+          );
+          return;
+        }
+        
+      
+        
+      })
+      .catch((error) => {
+        console.error("Error during payment verification:", error);
+      });
+    }
+    else
+    placeOrder();
+
+   
+  };
   const calculateSubtotal = () => {
     return cartItems.reduce(
       (subtotal, item) => subtotal + item.price * item.quantity,
@@ -67,7 +182,7 @@ const PlaceOrderScreen = ({ navigation, route }) => {
         .split(",")
         .map(parseFloat);
       const requests = riderEmailsArray.map(async (riderEmail) => {
-        console.log(riderEmail)
+        console.log(riderEmail);
         const response = await fetch(
           `http://${IP_ADDRESS}:5000/get-distance-of-rider/${riderEmail}/${latitude}/${longitude}`
         );
@@ -79,7 +194,7 @@ const PlaceOrderScreen = ({ navigation, route }) => {
 
       // Filter out results with errors (e.g., no rider found)
       const validResults = results.filter((result) => !result.error);
-      console.log(validResults)
+      console.log(validResults);
       if (validResults.length > 0) {
         const minDistanceRider = validResults.reduce((min, result) =>
           result.distance < min.distance ? result : min
@@ -96,14 +211,13 @@ const PlaceOrderScreen = ({ navigation, route }) => {
         setAssignedRider(minDistanceRider);
       }
     } catch (error) {
-      console.error("Error fetching rider distances:", error);
+      console.log("Error fetching rider distances:", error);
     } finally {
       setLoading(false);
     }
   };
 
   const placeOrder = async () => {
-    console.log(customerContact);
     const customerEmailResponse = await fetch(
       `http://${IP_ADDRESS}:5000/get-email`,
       {
@@ -159,6 +273,27 @@ const PlaceOrderScreen = ({ navigation, route }) => {
         });
 
         setLoading(false);
+        fetch(`http://${IP_ADDRESS}:5000/send-email-order`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ newOrder })
+})
+.then(response => {
+  if (response.ok) {
+    // Handle success response
+    console.log('Order email sent successfully');
+  } else {
+    // Handle error response
+    console.error('Failed to send order email');
+  }
+})
+.catch(error => {
+  // Handle network errors or exceptions
+  console.error('Error sending order email:', error);
+});
+
 
         // Now that the place order request is completed, make the notification request
         const notificationData = {
@@ -166,7 +301,7 @@ const PlaceOrderScreen = ({ navigation, route }) => {
           riderId: assignedRider.email,
           ownerId: store.ownerEmail,
           customerId: customerEmailData.email,
-          dateOfNotiifcation: new Date().toLocaleString()
+          dateOfNotiifcation: new Date().toLocaleString(),
         };
 
         const notificationResponse = await fetch(
@@ -180,8 +315,28 @@ const PlaceOrderScreen = ({ navigation, route }) => {
           }
         );
 
+
         if (notificationResponse.ok) {
-          
+          if (paymentMethod !== "COD") {
+            const name = new Date().getTime() + "_payment";
+            const type = "image/jpeg";
+            const formData = new FormData();
+            formData.append("profile", { name, uri: paymentProofImage, type });
+            formData.append("orderID", newOrder.orderID);
+            formData.append("mode", paymentMethod);
+            fetch(`http://${IP_ADDRESS}:5000/upload-payment`, {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-type": "multipart/form-data",
+              },
+              body: formData,
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data);
+              });
+          }
           navigation.navigate("OrderAnimation", {
             assignedRider,
             storeEndTime: store.endTime,
@@ -202,18 +357,31 @@ const PlaceOrderScreen = ({ navigation, route }) => {
         throw new Error("Failed to place order");
       }
     } catch (error) {
-      console.error("Error placing order:", error);
+      console.log("Error placing order:", error);
       setLoading(false);
     }
+  };
+  const handleUploadScreenshot = () => {
+    console.log("ann");
   };
   useEffect(() => {
     fetchRiderDistances();
   }, []); // Run only on component mount
 
+  // Function to handle payment method change
+  const handlePaymentMethodChange = (newValue) => {
+    setPaymentMethod(newValue);
+    if (newValue === "Jazzcash" || newValue === "Easypaisa") {
+      setIsPlaceOrderDisabled(true);
+    } else {
+      setIsPlaceOrderDisabled(false);
+    }
+  };
+
   return (
     <View style={{ flex: 1 }}>
       {/* Appbar/Header */}
-      <Appbar.Header>
+      <Appbar.Header style={{ backgroundColor: "#25d366" }}>
         <Appbar.BackAction onPress={() => navigation.goBack()} />
         <Appbar.Content title="Order Details" />
       </Appbar.Header>
@@ -229,77 +397,119 @@ const PlaceOrderScreen = ({ navigation, route }) => {
       )}
 
       {/* Cart Items */}
-      {!loading && (
+      {!loading  && (
         <ScrollView>
-          {cartItems.length === 0 ? (
-            <View style={styles.emptyCartContainer}>
-              <Text style={styles.emptyCartText}>Empty cart</Text>
-              <TouchableOpacity
-                onPress={() => navigation.replace("HomeCustomer")}
-              >
-                <Text style={styles.browseStoresText}>Browse to Stores</Text>
-              </TouchableOpacity>
+            {renderCartItemDetails()}
+          {cartItems.length > 0 && (
+            <View style={styles.subtotalContainer}>
+              <Text style={styles.subtotalText}>
+                Order Subtotal: ${calculateSubtotal()}
+              </Text>
             </View>
-          ) : (
-            renderCartItemDetails()
           )}
+
+          {/* Shipping Charges */}
+          {cartItems.length > 0 && (
+            <View style={styles.subtotalContainer}>
+              <Text style={styles.subtotalText}>
+                {loading
+                  ? "Shipping charges:  Calculating..."
+                  : `Shipping charges:  $${assignedRider.fee}`}
+              </Text>
+            </View>
+          )}
+
+          {/* Order Total */}
+          {cartItems.length > 0 && (
+            <View style={styles.subtotalContainer}>
+              <Text style={styles.subtotalText}>
+                Order Total:
+                {!loading
+                  ? ` $${calculateOrderTotal(
+                      calculateSubtotal(),
+                      assignedRider.fee
+                    )}`
+                  : "  Calculating..."}
+              </Text>
+            </View>
+          )}
+      {cartItems.length > 0 &&
+      <>
+          <Title style={styles.orderDetailsTitle}>Select Payment Method</Title>
+          <RadioButton.Group
+            onValueChange={handlePaymentMethodChange}
+            value={paymentMethod}
+          >
+            <View style={styles.radioButtonContainer}>
+              <RadioButton value="COD" />
+              <Text>Cash on Delivery</Text>
+            </View>
+            <View style={styles.radioButtonContainer}>
+              <RadioButton value="Jazzcash" />
+              <Text>Jazzcash</Text>
+            </View>
+            <View style={styles.radioButtonContainer}>
+              <RadioButton value="Easypaisa" />
+              <Text>Easypaisa</Text>
+            </View>
+          </RadioButton.Group>
+          
+      
+          {/* Payment Instructions */}
+          {paymentMethod === "Jazzcash" && (
+            <Text style={styles.paymentText}>
+              Our Jazzcash account No: 03217645512
+            </Text>
+          )}
+          {paymentMethod === "Easypaisa" && (
+            <Text style={styles.paymentText}>
+              Our Easypaisa account No: 03217645512
+            </Text>
+          )}
+          {paymentMethod !== "COD" && cartItems.length>0 && (
+            <TouchableOpacity onPress={handleUploadPicture}>
+              <Text style={styles.uploadText}>
+                Upload The Payment Screenshot
+              </Text>
+            </TouchableOpacity>
+          )}
+          {paymentProofImage && paymentMethod!=="COD" && (
+            <View style={styles.imageContainer}>
+              <Image source={{ uri: paymentProofImage }} style={styles.image} />
+            </View>
+            
+          )}
+      </>
+}  
         </ScrollView>
       )}
-
-      {/* Order Subtotal */}
-      {cartItems.length > 0 && (
-        <View style={styles.subtotalContainer}>
-          <Text style={styles.subtotalText}>
-            Order Subtotal: ${calculateSubtotal()}
-          </Text>
-        </View>
-      )}
-
-      {/* Shipping Charges */}
-      {cartItems.length > 0 && (
-        <View style={styles.subtotalContainer}>
-          <Text style={styles.subtotalText}>
-            {loading
-              ? "Shipping charges:  Calculating..."
-              : `Shipping charges:  $${assignedRider.fee}`}
-          </Text>
-        </View>
-      )}
-
-      {/* Order Total */}
-      {cartItems.length > 0 && (
-        <View style={styles.subtotalContainer}>
-          <Text style={styles.subtotalText}>
-            Order Total:
-            {!loading
-              ? ` $${calculateOrderTotal(
-                  calculateSubtotal(),
-                  assignedRider.fee
-                )}`
-              : "  Calculating..."}
-          </Text>
-        </View>
-      )}
-
-      {/* Display Rider Distances */}
-
-      {/* No Rider Available */}
       {!loading && Object.keys(riderDistances).length === 0 && (
         <View style={styles.subtotalContainer}>
           <Text style={styles.subtotalText}>No Rider's available</Text>
         </View>
       )}
-
       {/* Place Order Button */}
-      {!loading && cartItems.length > 0 && (
-        <Button
-          mode="contained"
-          style={styles.checkoutButton}
-          onPress={placeOrder}
-        >
-          Place Order
-        </Button>
-      )}
+      {!loading &&
+        cartItems.length > 0 &&
+        (
+            // Render Place Order Button
+            <Button
+              mode="contained"
+              style={[
+                styles.checkoutButton,
+                {
+                  backgroundColor: `${
+              "#25d366"
+                  }`,
+                },
+              ]}
+              onPress={handleVerifyPayment}
+            >
+              {isPaymentUploaded
+                ? "Verify Payment and Place Order"
+                : "Place Order"}
+            </Button>
+          )}
     </View>
   );
 };
